9a8d47b7c75ea8d2dd94ddd09bd022d3a8ea4f2d
yangw <yangwei957@163.com>
2019-12-27 16:38:06 +0800

    添加element支持

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueBus from 'vue-bus'
+import ElementUI from 'element-ui' // 引入ui
+import 'element-ui/lib/theme-chalk/index.css' // 引入ui
+import './style/main.less'
 
 Vue.use(VueBus)
+Vue.use(ElementUI, {size: 'small'}) // 引入ui
 
 Vue.config.productionTip = false
 
